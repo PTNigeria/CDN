431aaf94938b583893fa74a553cb5eca9c577711
Techie Nigeria Plc <PTNigeriaplc@gmail.com>
2017-07-20 16:21:33 +0100

    Techie Inc. Autos:- Thu 07/20/2017.

--- Techie.bat
+++ Techie.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 SET COMMITMESSAGE="Techie Inc. Autos:- %Date%".
 SET LOCALDIR=C:\WEB\WEBFILES\IYKEDAIRO\PTNigeria
-REM xcopy C:\Web\WEBFILES\config %LOCALDIR%\.git\config /G /Y
+xcopy C:\Web\WEBFILES\config %LOCALDIR%\.git\config /G /Y
 CD %LOCALDIR%
 
 
 git init
-git config global user.name PTNigeria
-git config global user.password iykesMan22
-git config global user.email PTNigeriaplc@gmail.com
+REM git config user.name PTNigeria
+REM git config user.password iykesMan22
+ git config user.email PTNigeriaplc@gmail.com
 git remote add Local https://github.com/PTNigeria/CDN.git
 git remote set-url --add Local https://github.com/PTNigeria/CDN.git
 git pull Local master --allow-unrelated-histories
@@ -16,7 +16,7 @@ git remote -v
 git add --all :/
 git commit -m %COMMITMESSAGE% :/
 git push Local master
- Rem xcopy C:\Web\WEBFILES\config %LOCALDIR%\.git\config /G /Y
+REM xcopy C:\Web\WEBFILES\config %LOCALDIR%\.git\config /G /Y
 
 PAUSE
 
