daa5246551ee368feba0b4bc471cea9a92fb777d
PTNigeria <PTNigeriaplc@gmail.com>
2017-07-20 16:18:20 +0100

    Techie Inc. Autos:- Thu 07/20/2017.

--- Techie.bat
+++ Techie.bat
@@ -6,9 +6,9 @@ CD %LOCALDIR%
 
 
 git init
-git config user.name PTNigeria
-git config user.password iykesMan22
-git config user.email PTNigeriaplc@gmail.com
+git config global user.name PTNigeria
+git config global user.password iykesMan22
+git config global user.email PTNigeriaplc@gmail.com
 git remote add Local https://github.com/PTNigeria/CDN.git
 git remote set-url --add Local https://github.com/PTNigeria/CDN.git
 git pull Local master --allow-unrelated-histories
