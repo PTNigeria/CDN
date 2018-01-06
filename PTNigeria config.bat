aa05cbaa5583ea081d122f2e618d3e5d310a5183
PTNigeria <PTNigeriaplc@gmail.com>
2018-01-06 12:17:45 +0100

    Techie Inc. Autos:- Sat 01/06/2018

--- PTNigeria config.bat
+++ PTNigeria config.bat
@@ -1,17 +1,13 @@
 @ECHO OFF
 SET LOCALDIR=C:\WEB\WEBFILES\IYKEDAIRO\PTNigeria
 CD %LOCALDIR%
-xcopy C:\Web\WEBFILES\config %LOCALDIR%\.git\config /G /Y
-
 git init
-rem cmdkey /delete:git:https//github.com
-rem git config --system --unset credential.helper
-git config credential.helper 'store'
+git config --global credential.helper wincred
 git config --local user.name PTNigeria
 git config --local user.password iykesMan22
 git config --local user.email PTNigeriaplc@gmail.com
-git remote add Local https://github.com/PTNigeria/CDN.git
-git remote set-url --add Local https://github.com/PTNigeria/CDN.git
+git remote add Local https://PTNigeria@github.com/PTNigeria/CDN.git
+git remote set-url --add Local https://PTNigeria@github.com/PTNigeria/CDN.git
 git pull Local master --allow-unrelated-histories
 git remote -v
 
