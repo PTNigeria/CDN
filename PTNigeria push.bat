8c4c88f2ab58f3252a58b41771ca0af147bc16ac
PTNigeria <PTNigeriaplc@gmail.com>
2018-01-10 02:56:17 +0100

    Techie Inc. Autos:- Wed 01/10/2018

--- PTNigeria push.bat
+++ PTNigeria push.bat
@@ -1,4 +1,6 @@
 SET COMMITMESSAGE="Techie Inc. Autos:- %Date%"
+   cmdkey /delete:https://github.com
+   cmdkey /generic:git:https://PTNigeria@github.com /user:PTNigeria /pass:iykesMan22
 git add --all :/
 git commit -m %COMMITMESSAGE% :/
 git push Local master
